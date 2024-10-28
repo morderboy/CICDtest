9f44c3d161ab456a55740388b920ca5adebf06d3
morderboy <slava137267@gmail.com>
2024-10-28 11:38:14 +1000

    init

---
+++ tests.py
@@ -0,0 +1,23 @@
+import unittest
+from fastapi.testclient import TestClient
+from main import app
+
+
+class FastApiTests(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.client = TestClient(app)
+
+    def test_read_root(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {"message": "Hello World"})
+
+    def test_read_hello(self):
+        response = self.client.get("/hello/User")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {"message": "Hello User"})
+
+
+if __name__ == '__main__':
+    unittest.main()
